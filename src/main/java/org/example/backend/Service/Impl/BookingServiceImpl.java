bd66d6599d80c0bcccab7b5308850f261501590b
Ivar Wennerholm <ivar.wennerholm@protonmail.com>
2024-04-26 16:24:25 +0200

    first commit

---
+++ src/main/java/org/example/backend/Service/Impl/BookingServiceImpl.java
@@ -0,0 +1,69 @@
+package org.example.backend.Service.Impl;
+
+import org.example.backend.DTO.BookingDto;
+import org.example.backend.DTO.CustomerDto;
+import org.example.backend.Model.Booking;
+import org.example.backend.Model.Customer;
+import org.example.backend.Model.Room;
+import org.example.backend.Repository.BookingRepository;
+import org.example.backend.Repository.CustomerRepository;
+import org.example.backend.Repository.RoomRepository;
+import org.example.backend.Service.BookingService;
+import org.example.backend.Service.CustomerService;
+
+import java.util.List;
+
+public class BookingServiceImpl implements BookingService {
+
+    private RoomService roomService;
+    private CustomerService customerService;
+    private RoomRepository roomRepository;
+    private CustomerRepository customerRepository;
+    private BookingRepository bookingRepository;
+
+    @Override
+    public BookingDto bookingToBookingDto(Booking b) {
+        RoomDto roomDto = roomService.roomToRoomDto(roomRepository.getReferenceById(b.getRoom().getId()));
+        CustomerDto customerDto = customerService.customerToCustomerDto(customerRepository.getReferenceById(b.getCustomer().getId()));
+        return BookingDto.builder().
+                id(b.getId()).
+                checkinDate(b.getCheckinDate()).
+                checkoutDate(b.getCheckoutDate()).
+                guestAmt(b.getGuestAmt()).
+                extraBedAmt(b.getExtraBedAmt()).
+                customer(customerDto).
+                room(roomDto).
+                build();
+    }
+
+    @Override
+    public Booking bookindDtoToBooking(BookingDto bd) {
+        Room room = roomService.roomDtoToRoom(bd.getRoom());
+        Customer customer = customerService.customerDtoToCustomer(bd.getCustomer());
+        return Booking.builder().
+                id(bd.getId()).
+                checkinDate(bd.getCheckinDate()).
+                checkoutDate(bd.getCheckoutDate()).
+                guestAmt(bd.getGuestAmt()).
+                extraBedAmt(bd.getExtraBedAmt()).
+                customer(customer).
+                room(room).
+                build();
+    }
+
+    @Override
+    public List<BookingDto> getAll() {
+        return bookingRepository.findAll().stream().map(this::bookingToBookingDto).toList();
+    }
+
+    @Override
+    public void deleteBooking(BookingDto bd) {
+        bookingRepository.deleteById(bd.getId());
+    }
+
+    @Override
+    public void updateBooking(BookingDto bd) {
+        bookingRepository.deleteById(bd.getId());
+        bookingRepository.save(bookindDtoToBooking(bd));
+    }
+}
