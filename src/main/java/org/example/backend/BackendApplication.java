3e6c31854f3ead6f9d6a2398da4b7e7750b5d46c
Venus Poon <venuspsw@hotmail.com>
2024-04-29 14:36:08 +0200

    resume @Bean

--- src/main/java/org/example/backend/BackendApplication.java
+++ src/main/java/org/example/backend/BackendApplication.java
@@ -23,50 +23,47 @@ public class BackendApplication {
         SpringApplication.run(BackendApplication.class, args);
     }
 
-//    @Bean
-//    public CommandLineRunner commandLineRunner(CustomerRepository cRepo, RoomTypeRepository rtRepo, RoomRepository rRepo, BookingRepository bRepo) {
-//        return (args) -> {
-//            // Customers
-//            Customer c1 = new Customer("Venus", "111-1111111");
-//            Customer c2 = new Customer("Alex", "222-2222222");
-//            Customer c3 = new Customer("Ivar", "333-3333333");
-//            cRepo.save(c1);
-//            cRepo.save(c2);
-//            cRepo.save(c3);
-//
-//            // Room Types
-//            RoomType rt1 = new RoomType(1L, "single", 0, 1,500);
-//            RoomType rt2 = new RoomType(2L, "double", 1, 3,1000);
-//            RoomType rt3 = new RoomType(3L, "large_double", 2, 4,1500);
-//            rtRepo.save(rt1);
-//            rtRepo.save(rt2);
-//            rtRepo.save(rt3);
-//
-//            // Rooms
-//            Room r1 = new Room(2099, rt1);
-//            Room r2 = new Room( 3049, rt2);
-//            Room r3 = new Room(3540, rt3);
-//            rRepo.save(r1);
-//            rRepo.save(r2);
-//            rRepo.save(r3);
-//
-//            // Bookings
-//            DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-//            Booking b1 = new Booking(new java.sql.Date(df.parse("2024-06-01").getTime()),
-//                    new java.sql.Date(df.parse("2024-06-07").getTime()), 1, 0, c1, r1);
-//            Booking b2 = new Booking(new java.sql.Date(df.parse("2024-08-22").getTime()),
-//                    new java.sql.Date(df.parse("2024-08-23").getTime()), 3, 1, c2, r2);
-//            Booking b3 = new Booking(new java.sql.Date(df.parse("2024-12-23").getTime()),
-//                    new java.sql.Date(df.parse("2024-12-25").getTime()), 4, 2, c3, r3);
-//            Booking b4 = new Booking(new java.sql.Date(df.parse("2024-12-23").getTime()),
-//                    new java.sql.Date(df.parse("2024-12-30").getTime()), 3, 1, c2, r2);
-//            bRepo.save(b1);
-//            bRepo.save(b2);
-//            bRepo.save(b3);
-//            bRepo.save(b4);
-//
-//
-//        };
-//    }
+    @Bean
+    public CommandLineRunner commandLineRunner(CustomerRepository cRepo, RoomTypeRepository rtRepo, RoomRepository rRepo, BookingRepository bRepo) {
+        return (args) -> {
+            // Customers
+            Customer c1 = new Customer("Venus", "111-1111111");
+            Customer c2 = new Customer("Alex", "222-2222222");
+            Customer c3 = new Customer("Ivar", "333-3333333");
+            cRepo.save(c1);
+            cRepo.save(c2);
+            cRepo.save(c3);
 
+            // Room Types
+            RoomType rt1 = new RoomType(1L, "single", 0, 1,500);
+            RoomType rt2 = new RoomType(2L, "double", 1, 3,1000);
+            RoomType rt3 = new RoomType(3L, "large_double", 2, 4,1500);
+            rtRepo.save(rt1);
+            rtRepo.save(rt2);
+            rtRepo.save(rt3);
+
+            // Rooms
+            Room r1 = new Room(2099, rt1);
+            Room r2 = new Room( 3049, rt2);
+            Room r3 = new Room(3540, rt3);
+            rRepo.save(r1);
+            rRepo.save(r2);
+            rRepo.save(r3);
+
+            // Bookings
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+            Booking b1 = new Booking(new java.sql.Date(df.parse("2024-06-01").getTime()),
+                    new java.sql.Date(df.parse("2024-06-07").getTime()), 1, 0, c1, r1);
+            Booking b2 = new Booking(new java.sql.Date(df.parse("2024-08-22").getTime()),
+                    new java.sql.Date(df.parse("2024-08-23").getTime()), 3, 1, c2, r2);
+            Booking b3 = new Booking(new java.sql.Date(df.parse("2024-12-23").getTime()),
+                    new java.sql.Date(df.parse("2024-12-25").getTime()), 4, 2, c3, r3);
+            Booking b4 = new Booking(new java.sql.Date(df.parse("2024-12-23").getTime()),
+                    new java.sql.Date(df.parse("2024-12-30").getTime()), 3, 1, c2, r2);
+            bRepo.save(b1);
+            bRepo.save(b2);
+            bRepo.save(b3);
+            bRepo.save(b4);
+        };
+    }
 }
