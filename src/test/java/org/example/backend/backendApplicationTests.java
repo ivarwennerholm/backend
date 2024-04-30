e6418ae9ec570e056ad3c8799750f5f70cfcbf65
Venus Poon <venuspsw@hotmail.com>
2024-04-30 09:32:45 +0200

    add test directory

---
+++ src/test/java/org/example/backend/backendApplicationTests.java
@@ -0,0 +1,11 @@
+package org.example.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class backendApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
