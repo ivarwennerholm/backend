9a4f2a5f65cfc44b2d9c145482ca3f50e034fa73
Ivar Wennerholm <ivar.wennerholm@protonmail.com>
2024-05-03 14:51:15 +0200

    expanded error messages to cover all possible combinations

--- src/main/resources/static/js/searchBooking.js
+++ src/main/resources/static/js/searchBooking.js
@@ -1,21 +1,37 @@
 function validateDates() {
     var checkin = document.getElementById("checkin").value;
     var checkout = document.getElementById("checkout").value;
-    if (checkin === "" || checkout === "") {
+    var checkinDate;
+    var checkoutDate;
+    if (checkin === "" && checkout === "") {
         return false;
     }
-    var checkinDate = new Date(checkin);
-    var checkoutDate = new Date(checkout);
+    if (checkin !== "" && checkout === "") {
+        checkinDate = new Date(checkin);
+        if (checkinDate < today) {
+            markup = "<p class=\"text-secondary\"><small><small>⚠️ Checkin date must be no earlier than tomorrow</small></small></p>";
+            document.getElementById("date-error").innerHTML = markup;
+            console.log("checkinDate < today")
+            return false;
+        }
+    }
+    if (checkin === "" && checkout !== "") {
+            return false;
+    }
+    checkinDate = new Date(checkin);
+    checkoutDate = new Date(checkout);
     var today = new Date();
-    if (checkoutDate <= checkinDate) {
+    if (checkoutDate <= checkinDate && checkinDate < today) {
+        markup = "<p class=\"text-secondary\"><small><small>⚠️️ Checkout date must be after checkin date<br>⚠️ Checkin date must be no earlier than tomorrow</small></small></p>";
+        document.getElementById("date-error").innerHTML = markup;
+        return false;
+    } else if (checkoutDate <= checkinDate) {
         markup = "<p class=\"text-secondary\"><small><small>⚠️️ Checkout date must be after checkin date</small></small></p>";
         document.getElementById("date-error").innerHTML = markup;
-        console.log("checkoutDate <= checkinDate")
         return false;
     } else if (checkinDate < today) {
         markup = "<p class=\"text-secondary\"><small><small>⚠️ Checkin date must be no earlier than tomorrow</small></small></p>";
         document.getElementById("date-error").innerHTML = markup;
-        console.log("checkinDate < today")
         return false;
     } else {
         markup = "";
