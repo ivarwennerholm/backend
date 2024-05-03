abdc9d1342639c7ab21689c132a99242d5821075
Ivar Wennerholm <ivar.wennerholm@protonmail.com>
2024-05-03 18:43:32 +0200

    some tests working

---
+++ src/test/java/org/example/backend/BookingServiceTest2.java
@@ -0,0 +1,228 @@
+package org.example.backend;
+
+import org.example.backend.Model.Booking;
+import org.example.backend.Model.Customer;
+import org.example.backend.Model.Room;
+import org.example.backend.Model.RoomType;
+import org.example.backend.Repository.BookingRepository;
+import org.example.backend.Service.BookingService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class BookingServiceTest2 {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private BookingService bookingService;
+
+    @MockBean
+    private BookingRepository mockRepo;
+
+    private DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+
+    // Items for repo:
+    // Customers
+    Customer c1 = new Customer("Venus", "111-1111111");
+    Customer c2 = new Customer("Alex", "222-2222222");
+    Customer c3 = new Customer("Ivar", "333-3333333");
+
+    // Room Types
+    RoomType rt1 = new RoomType(1L, "Single", 0, 1, 500);
+    RoomType rt2 = new RoomType(2L, "Double", 1, 3, 1000);
+    RoomType rt3 = new RoomType(3L, "Large double", 2, 4, 1500);
+
+    // Rooms
+    Room r1 = new Room(101, rt1);
+    Room r2 = new Room(102, rt2);
+    Room r3 = new Room(103, rt3);
+
+    // Bookings
+    Booking b1 = new Booking(1L, new java.sql.Date(df.parse("2024-06-01").getTime()),
+            new java.sql.Date(df.parse("2024-06-07").getTime()), 1, 0, c1, r1);
+    Booking b2 = new Booking(2L, new java.sql.Date(df.parse("2024-08-22").getTime()),
+            new java.sql.Date(df.parse("2024-08-23").getTime()), 3, 1, c2, r2);
+    Booking b3 = new Booking(3L, new java.sql.Date(df.parse("2024-12-23").getTime()),
+            new java.sql.Date(df.parse("2024-12-25").getTime()), 4, 2, c3, r3);
+
+    // Ansi colors for readability
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+
+    public BookingServiceTest2() throws ParseException {
+    }
+
+    @BeforeEach
+    public void init() throws ParseException {
+        when(mockRepo.findById(1L)).thenReturn(Optional.of(b1));
+        when(mockRepo.findById(2L)).thenReturn(Optional.of(b2));
+        when(mockRepo.findById(3L)).thenReturn(Optional.of(b3));
+        when(mockRepo.findAll()).thenReturn(Arrays.asList(b1, b2, b3));
+        // System.out.println(ANSI_GREEN + "@BeforeEach completed" + ANSI_RESET); // FOR READABILITY ONLY
+    }
+
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(bookingService).isNotNull();
+        assertThat(mockRepo).isNotNull();
+    }
+
+    @Test
+    public void checkMockRepo() throws ParseException {
+        Booking expected1 = b1;
+        Booking expected2 = b2;
+        Booking expected3 = b3;
+        Optional<Booking> optional1 = mockRepo.findById(1L);
+        Optional<Booking> optional2 = mockRepo.findById(2L);
+        Optional<Booking> optional3 = mockRepo.findById(3L);
+        List<Booking> actualList = mockRepo.findAll();
+        if (optional1.isPresent() && optional2.isPresent() && optional3.isPresent()) {
+            Booking actual1 = optional1.get();
+            Booking actual2 = optional2.get();
+            Booking actual3 = optional3.get();
+            List<Booking> expectedList = Arrays.asList(b1, b2, b3);
+            Assertions.assertEquals(actual1, expected1);
+            Assertions.assertEquals(actual2, expected2);
+            Assertions.assertEquals(actual3, expected3);
+            Assertions.assertEquals(actualList, expectedList);
+        } else {
+            Assertions.fail("Booking not found");
+        }
+    }
+
+    @Test
+    public void testCreateAndAddBookingToDatabase() {
+        /*
+        public void createAndAddBookingToDatabase(Date checkin, Date checkout, int guests, int extraBeds, long roomId, String name, String phone) {
+        customerService.addCustomerWithoutID(name, phone);
+        Customer customer = customerService.getCustomerByNameAndPhone(name, phone);
+        Room room = roomRepository.findById(roomId).orElse(null);
+        Booking booking = new Booking(checkin, checkout, guests, extraBeds, customer, room);
+        bookingRepository.save(booking);
+    }
+    */
+    }
+
+    @Test // WORKING
+    public void testAreDatesOverlapping() throws ParseException {
+        Date d1 = bookingService.convertStringToDate("2024-07-01");
+        Date d2 = bookingService.convertStringToDate("2024-07-30");
+        Date d3 = bookingService.convertStringToDate("2024-07-31");
+        Date d4 = bookingService.convertStringToDate("2024-08-01");
+        Date d5 = bookingService.convertStringToDate("2024-08-03");
+        Date d6 = bookingService.convertStringToDate("2024-08-04");
+        Date d7 = bookingService.convertStringToDate("2024-08-16");
+        List<Date> search = bookingService.createDateInterval(d3, d5);
+        List<Date> notOverLapping1 = bookingService.createDateInterval(d1, d2);
+        List<Date> notOverLapping2 = bookingService.createDateInterval(d1, d3);
+        List<Date> notOverLapping3 = bookingService.createDateInterval(d5, d7);
+        List<Date> notOverLapping4 = bookingService.createDateInterval(d6, d7);
+        List<Date> overLapping1 = bookingService.createDateInterval(d1, d4);
+        List<Date> overLapping2 = bookingService.createDateInterval(d4, d7);
+        Assertions.assertTrue(bookingService.areDatesOverlapping(overLapping1, search));
+        Assertions.assertTrue(bookingService.areDatesOverlapping(overLapping2, search));
+        Assertions.assertTrue(bookingService.areDatesOverlapping(search, search));
+        Assertions.assertFalse(bookingService.areDatesOverlapping(notOverLapping1, search));
+        Assertions.assertFalse(bookingService.areDatesOverlapping(notOverLapping2, search));
+        Assertions.assertFalse(bookingService.areDatesOverlapping(notOverLapping3, search));
+        Assertions.assertFalse(bookingService.areDatesOverlapping(notOverLapping4, search));
+    }
+
+    @Test // WORKING
+    public void testCreateDateInterval() throws ParseException {
+        Date d1 = bookingService.convertStringToDate("2024-07-31");
+        Date d2 = bookingService.convertStringToDate("2024-08-01");
+        Date d3 = bookingService.convertStringToDate("2024-08-02");
+        Date d4 = bookingService.convertStringToDate("2024-08-03");
+        List<Date> expected = Arrays.asList(d1, d2, d3, d4);
+        List<Date> actual = bookingService.createDateInterval(d1, d4);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetNumberOfDaysBetweenTwoDates() {
+    /*
+    public Long getNumberOfDaysBetweenTwoDates(Date checkin, Date checkout) {
+        long differenceMillis = checkout.getTime() - checkin.getTime();
+        return differenceMillis / (1000 * 60 * 60 * 24);
+    }
+    */
+    }
+
+    @Test
+    public void testGetExtraBedsForBooking() {
+    /*
+    public int getExtraBedsForBooking(RoomDto room, int guests) {
+        int beds = 0;
+        switch (room.getRoomType().getType()) {
+            case "Single":
+                break;
+            case "Double":
+                if (guests == 3) {
+                    beds = 1;
+                    break;
+                } else
+                    break;
+            case "Large double":
+                if (guests == 4) {
+                    beds = 2;
+                    break;
+                } else if (guests == 3) {
+                    beds = 1;
+                    break;
+                } else
+                    break;
+        }
+        return beds;
+    }
+    */
+    }
+
+    @Test
+    public void testIsRoomAvailableOnDates() {
+
+        /*
+        public boolean isRoomAvailableOnDates(RoomDto room, Date checkin, Date checkout) {
+        long roomId = room.getId();
+        List<Date> datesInterval = createDateInterval(checkin, checkout);
+        List<BookingDto> conflictingBookings = getAll().
+                stream().
+                filter(b -> b.getRoom().getId() == roomId).
+                filter(b -> areDatesOverlapping(datesInterval, createDateInterval(b.getCheckinDate(), b.getCheckoutDate()))).
+                toList();
+        return conflictingBookings.isEmpty();
+        */
+    }
+
+    @Test // WORKING
+    public void testConvertStringToDate() throws ParseException {
+        String string = "2024-06-01";
+        java.sql.Date expected = new java.sql.Date(df.parse(string).getTime());
+        java.sql.Date actual = bookingService.convertStringToDate(string);
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
