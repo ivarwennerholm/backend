02c58513b9177a0ec2e2161117ff25655c8652d7
Ivar Wennerholm <ivar.wennerholm@protonmail.com>
2024-05-07 18:13:10 +0200

    added AllContractCustomers collection class

---
+++ src/main/java/org/example/backend/Model/AllContractCustomers.java
@@ -0,0 +1,12 @@
+package org.example.backend.Model;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+import java.util.List;
+
+@JacksonXmlRootElement(localName = "allcustomers")
+public class AllContractCustomers {
+     @JacksonXmlProperty(localName = "customers")
+     public List<ContractCustomer> contractCustomers;
+}
