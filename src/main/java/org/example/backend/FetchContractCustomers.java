825a66cdf2893166178cebe88a4d00fffe69b07b
Ivar Wennerholm <ivar.wennerholm@protonmail.com>
2024-05-07 22:20:17 +0200

    implemented try-catch and logger in FetchContractCustomers.java

--- src/main/java/org/example/backend/FetchContractCustomers.java
+++ src/main/java/org/example/backend/FetchContractCustomers.java
@@ -6,6 +6,8 @@ import org.example.backend.Model.AllContractCustomers;
 import org.example.backend.Model.ContractCustomer;
 import org.example.backend.Repository.ContractCustomerRepository;
 import org.example.backend.Service.ContractCustomerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.ComponentScan;
@@ -21,24 +23,24 @@ public class FetchContractCustomers implements CommandLineRunner {
     @Autowired
     ContractCustomerService service;
 
-    // ANSI colors for readability
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
+    private static final Logger logger = LoggerFactory.getLogger(FetchContractCustomers.class);
 
     @Override
-    public void run(String... args) throws Exception {
-        System.out.println(ANSI_YELLOW + "Running FetchContractCustomers" + ANSI_RESET); // FOR TESTING ONLY
-        JacksonXmlModule module = new JacksonXmlModule();
-        module.setDefaultUseWrapper(false);
-        XmlMapper xmlMapper = new XmlMapper(module);
-        AllContractCustomers allContractCustomers = xmlMapper.readValue(new URL("https://javaintegration.systementor.se/customers"),
-                AllContractCustomers.class
-        );
-        repo.deleteAll();
-        for (ContractCustomer cc : allContractCustomers.list){
-            repo.save(cc);
+    public void run(String... args) {
+        logger.info("Running FetchContractCustomers");
+        try {
+            JacksonXmlModule module = new JacksonXmlModule();
+            module.setDefaultUseWrapper(false);
+            XmlMapper xmlMapper = new XmlMapper(module);
+            AllContractCustomers allContractCustomers = xmlMapper.readValue(new URL("https://javaintegration.systementor.se/customers"),
+                    AllContractCustomers.class
+            );
+            repo.deleteAll();
+            for (ContractCustomer cc : allContractCustomers.list) {
+                repo.save(cc);
+            }
+        } catch (Exception e) {
+            logger.error("FetchContractCustomer: An error occurred during execution", e);
         }
     }
 
