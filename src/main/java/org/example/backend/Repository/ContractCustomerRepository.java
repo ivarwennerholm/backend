7c857f3f6fe301c56dca4459cb35aa5548fd4ee4
Ivar Wennerholm <ivar.wennerholm@protonmail.com>
2024-05-07 17:49:23 +0200

    added ContractCustomerRepository

---
+++ src/main/java/org/example/backend/Repository/ContractCustomerRepository.java
@@ -0,0 +1,6 @@
+package org.example.backend.Repository;
+
+import org.example.backend.Model.ContractCustomer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContractCustomerRepository extends JpaRepository<ContractCustomer, Long> {}
