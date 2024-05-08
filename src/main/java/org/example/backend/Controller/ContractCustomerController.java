d69d0ada211e2d4bb0a9fe0551140c412a5da43b
Ivar Wennerholm <ivar.wennerholm@protonmail.com>
2024-05-08 12:21:09 +0200

    working list of all CCs

--- src/main/java/org/example/backend/Controller/ContractCustomerController.java
+++ src/main/java/org/example/backend/Controller/ContractCustomerController.java
@@ -5,6 +5,8 @@ import org.example.backend.DTO.ContractCustomerDto;
 import org.example.backend.Service.ContractCustomerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -16,16 +18,16 @@ public class ContractCustomerController {
 
     private final ContractCustomerService service;
 
-    @RequestMapping("/")
-        public String getAllContractCustomer (Model model){
+    @GetMapping
+    public String getAllContractCustomer(Model model) {
         List<ContractCustomerDto> list = service.getAll();
-            model.addAttribute("list", list);
-            return "allContractCustomers.html";
-        }
+        model.addAttribute("list", list);
+        return "allContractCustomers.html";
+    }
 
-    @RequestMapping("test")
-    public List<ContractCustomerDto> test (){
-        return service.getAll();
+    @GetMapping("/{id}")
+    public String getContractCustomer(@PathVariable Long id, Model model) {
+        return null;
     }
 
 }
