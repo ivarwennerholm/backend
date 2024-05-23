0defd08983329422756fcb17b657ccd1861f0889
Ivar Wennerholm <ivar.wennerholm@protonmail.com>
2024-05-23 06:48:47 +0200

    working integrations test 1/2

--- src/test/java/org/example/backend/BookingServiceIT.java
+++ src/test/java/org/example/backend/BookingServiceIT.java
@@ -1,23 +1,19 @@
 package org.example.backend;
 
-import org.example.backend.Model.Booking;
 import org.example.backend.Model.Customer;
 import org.example.backend.Model.Room;
 import org.example.backend.Model.RoomType;
 import org.example.backend.Repository.BookingRepository;
 import org.example.backend.Repository.CustomerRepository;
 import org.example.backend.Repository.RoomRepository;
-import org.example.backend.Repository.RoomTypeRepository;
 import org.example.backend.Service.BookingService;
 import org.example.backend.Service.Impl.*;
-import org.example.backend.Service.RoomService;
-import org.example.backend.Service.RoomTypeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.sql.Date;
@@ -26,38 +22,25 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-@SpringBootTest
 public class BookingServiceIT {
 
     @Mock
-    private RoomTypeRepository roomTypeRepository;
-
-    @Mock
-    private RoomTypeService roomTypeService;
-    // = new RoomTypeServiceImpl(roomTypeRepository);
-
-    @Mock
-    private RoomRepository roomRepository;
-
-    @Mock
-    private RoomService roomService;
-    // = new RoomServiceImpl(roomRepository, roomTypeRepository, roomTypeService);
-
-    @Mock
-    private CustomerRepository customerRepository;
+    private RoomServiceImpl roomService;
 
     @Mock
     private CustomerServiceImpl customerService;
 
     @Mock
-    private BookingRepository bookingRepository;
+    private RoomRepository roomRepository;
 
     @Mock
-    private DateService dateService;
+    private CustomerRepository customerRepository;
+
+    @Mock
+    private BookingRepository bookingRepository;
 
     @Mock
     private BlacklistService blacklistService;
@@ -66,13 +49,7 @@ public class BookingServiceIT {
     private DiscountService discountService;
 
     @InjectMocks
-    private BookingService sut1;
-    //= new BookingServiceImpl(roomService, customerService, roomRepository, customerRepository, bookingRepository, blacklistService, discountService);
-
-    //private final BookingService sut1 = new BookingServiceImpl(roomService, customerService, roomRepository, customerRepository, bookingRepository, blacklistService, discountService);
-
-    //@Autowired
-    //private BookingRepository sut2;
+    private BookingServiceImpl bookingService;
 
     private Customer customer;
     private RoomType roomType;
@@ -95,6 +72,7 @@ public class BookingServiceIT {
 
     @Test
     public void testCreateAndAddBookingToDatabase() throws Exception {
+
         // ARRANGE
         Date checkin = new Date(df.parse("2024-06-01").getTime());
         Date checkout = new Date(df.parse("2024-06-07").getTime());
@@ -108,18 +86,17 @@ public class BookingServiceIT {
         when(customerService.getCustomerByNamePhoneAndEmail(anyString(), anyString(), anyString())).thenReturn(Optional.of(customer));
         when(blacklistService.isEmailValid(anyString())).thenReturn(true);
         System.out.println("isEmailValid: " + blacklistService.isEmailValid("test"));
-        //System.out.println("isEmailValid: " + sut1.blacklistService.isEmailValid("test"));
+        //System.out.println("isEmailValid: " + bookingService.blacklistService.isEmailValid("test"));
 
-        when(dateService.getNumberOfDaysBetweenTwoDates(any(Date.class), any(Date.class))).thenReturn(7L);
+        //when(dateService.getNumberOfDaysBetweenTwoDates(any(Date.class), any(Date.class))).thenReturn(7L);
         when(discountService.getTotalPriceWithDiscounts(any(Date.class), any(Date.class), anyLong(), anyLong(), any(Date.class), anyBoolean())).thenReturn(12000D);
         when(roomRepository.findById(anyLong())).thenReturn(Optional.of(room));
 
         // ACT
-        sut1.createAndAddBookingToDatabase(checkin, checkout, guests, extraBeds, roomId, customerName, customerPhone, customerEmail);
+        bookingService.createAndAddBookingToDatabase(checkin, checkout, guests, extraBeds, roomId, customerName, customerPhone, customerEmail);
 
         // ASSERT
         verify(bookingRepository, times(1)).save(any());
-    }
 
         /*
         // ARRANGE
@@ -134,8 +111,9 @@ public class BookingServiceIT {
         String customerEmail = customer.getEmail();
 
         when(customerRepository.getCustomerByNamePhoneAndEmail(anyString(), anyString(), anyString())).thenReturn(Optional.ofNullable(customer));
-        when(customerService.getCustomerByNamePhoneAndEmail(customerName, customerPhone, customerEmail)).thenReturn(Optional.ofNullable(customer));
-        doReturn(7L).when(dateService).getNumberOfDaysBetweenTwoDates(any(Date.class), any(Date.class));
+        when(customerService.getCustomerByNamePhoneAndEmail(anyString(), anyString(), anyString())).thenReturn(Optional.ofNullable(customer));
+        //doReturn(7L).when(dateService).getNumberOfDaysBetweenTwoDates(any(Date.class), any(Date.class));
+        when(blacklistService.isEmailValid(anyString())).thenReturn(true);
 
         when(discountService.getTotalPriceWithDiscounts(any(Date.class), any(Date.class), anyLong(), anyLong(), any(Date.class), anyBoolean())).thenReturn(12000D);
         optional = customerService.getCustomerByNamePhoneAndEmail(customerName, customerPhone, customerEmail);
@@ -146,11 +124,12 @@ public class BookingServiceIT {
         // bookingRepository.deleteAll();
 
         // ACT
-        sut.createAndAddBookingToDatabase(checkin, checkout, guests, extraBeds, roomId, customerName, customerPhone, customerEmail);
+        sut1.createAndAddBookingToDatabase(checkin, checkout, guests, extraBeds, roomId, customerName, customerPhone, customerEmail);
 
         // ASSURE
         verify(bookingRepository, times(1)).save(any());
         System.out.println(ANSI_GREEN + "Number of entries in database: " + bookingRepository.findAll().size() + ANSI_RESET);
+
         /*
         List<Booking> list = bookingRepository.findAll().stream().toList();
         for (Booking booking : list) {
@@ -158,26 +137,24 @@ public class BookingServiceIT {
         }
         */
 
-    // assertEquals(1, (long) bookingRepository.findAll().size());
-    // verify(bookingRepository.times(3).).save
+        // assertEquals(1, (long) bookingRepository.findAll().size());
+        // verify(bookingRepository.times(3).).save
 
-    // Verification
-    // verify(customerService).getCustomerByNamePhoneAndEmail(customerName, customerPhone, customerEmail);
-    // verify(roomRepository).findById(roomId);
-    // verify(bookingRepository).save(any(Booking.class)); // Ensure that the save method is called with a Booking object
+        // Verification
+        // verify(customerService).getCustomerByNamePhoneAndEmail(customerName, customerPhone, customerEmail);
+        // verify(roomRepository).findById(roomId);
+        // verify(bookingRepository).save(any(Booking.class)); // Ensure that the save method is called with a Booking object
 
-    // Date checkin = new java.sql.Date(df.parse("2024-06-01").getTime());
-    // Date checkout = new java.sql.Date(df.parse("2024-06-07").getTime());
-    // int guestAmt = 1;
-    // int extraBedAmt = 0;
-    // Room room = r1;
-    // Long roomId = room.getId();
-    // Customer customer = c1;
-    // String name = customer.getName();
-    // String phone = customer.getPhone();
-    // Booking booking = new Booking(checkin, checkout, guestAmt, extraBedAmt, customer, room);
-
-
+        // Date checkin = new java.sql.Date(df.parse("2024-06-01").getTime());
+        // Date checkout = new java.sql.Date(df.parse("2024-06-07").getTime());
+        // int guestAmt = 1;
+        // int extraBedAmt = 0;
+        // Room room = r1;
+        // Long roomId = room.getId();
+        // Customer customer = c1;
+        // String name = customer.getName();
+        // String phone = customer.getPhone();
+        // Booking booking = new Booking(checkin, checkout, guestAmt, extraBedAmt, customer, room);
+    }
 
 }
-
