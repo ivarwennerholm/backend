50e285de091e364374c1ee3d0ee1cd32dad7f003
Ivar Wennerholm <ivar.wennerholm@protonmail.com>
2024-05-28 22:11:55 +0200

    cleaned repos

--- src/main/java/org/example/backend/Repository/CustomerRepository.java
+++ src/main/java/org/example/backend/Repository/CustomerRepository.java
@@ -7,11 +7,11 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    public Customer findByName(String name);
+    Customer findByName(String name);
 
     @Query("SELECT c FROM Customer c WHERE c.name = :name AND c.phone = :phone AND c.email = :email")
-    public Optional<Customer> getCustomerByNamePhoneAndEmail(String name, String phone, String email);
+    Optional<Customer> getCustomerByNamePhoneAndEmail(String name, String phone, String email);
 
     @Query("SELECT c FROM Customer c ORDER BY c.id DESC LIMIT 1")
-    public Customer getLastCustomer();
+    Customer getLastCustomer();
 }
