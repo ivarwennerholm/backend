4e1e2ff972fb1215e6bd8b4bc5fe0547e02d22de
Ivar Wennerholm <ivar.wennerholm@protonmail.com>
2024-05-29 13:16:57 +0200

    updated database-init.sql

--- database-init.sql
+++ database-init.sql
@@ -5,7 +5,7 @@ CREATE USER 'hoteluser'@'localhost' IDENTIFIED BY 'SuperSecretPassword';
 GRANT ALL PRIVILEGES ON Hotel.* TO 'hoteluser'@'localhost' WITH GRANT OPTION;
 
 # Development config
-CREATE DATABASE Hotel_Development
+CREATE DATABASE Hotel_Development;
 SET GLOBAL validate_password.policy=LOW;
 CREATE USER 'developer'@'localhost' IDENTIFIED BY 'IAmADeveloper';
 GRANT ALL PRIVILEGES ON Hotel_Development.* TO 'developer'@'localhost' WITH GRANT OPTION;
